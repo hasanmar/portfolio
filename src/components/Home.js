67392b575b6efce8c67c1b0588108dc994d5303f
Hasan Marhoon <hasanmarhoon98@gmail.com>
2023-05-15 14:33:08 +0300

    moodified navbar expand, styling and added whatsapp link

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,31 +55,36 @@ export default function Home() {
                     <ProgressBarComponent skill='Enterprise Architect' now='90' label='90%' />
                 </div>
                 <div id='home-bottom'>
-                    <h2><span className='highlight'>Languages</span></h2>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-1-square-fill" viewBox="0 0 16 16">
-                        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm7.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z" />
-                    </svg> Arabic (native)</p>
-                    <p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-2-square-fill" viewBox="0 0 16 16">
-                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm4.646 6.24v.07H5.375v-.064c0-1.213.879-2.402 2.637-2.402 1.582 0 2.613.949 2.613 2.215 0 1.002-.6 1.667-1.287 2.43l-.096.107-1.974 2.22v.077h3.498V12H5.422v-.832l2.97-3.293c.434-.475.903-1.008.903-1.705 0-.744-.557-1.236-1.313-1.236-.843 0-1.336.615-1.336 1.306Z" />
-                        </svg> English (fluent)
-                    </p>
-                    <h2><span className='highlight'>Hobbies </span></h2>
-                    <p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-square-fill" viewBox="0 0 16 16">
-                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
-                        </svg> Listening to and discovering music
-                    </p>
-                    <p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-square-fill" viewBox="0 0 16 16">
-                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
-                        </svg> Sports (football, padel, table tennis)
-                    </p>
-                    <p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-square-fill" viewBox="0 0 16 16">
-                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
-                        </svg> Gaming
-                    </p>
+                    <div>
+                        <h2><span className='highlight'>Languages</span></h2>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-1-square-fill" viewBox="0 0 16 16">
+                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm7.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z" />
+                        </svg> Arabic (native)</p>
+                        <p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-2-square-fill" viewBox="0 0 16 16">
+                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm4.646 6.24v.07H5.375v-.064c0-1.213.879-2.402 2.637-2.402 1.582 0 2.613.949 2.613 2.215 0 1.002-.6 1.667-1.287 2.43l-.096.107-1.974 2.22v.077h3.498V12H5.422v-.832l2.97-3.293c.434-.475.903-1.008.903-1.705 0-.744-.557-1.236-1.313-1.236-.843 0-1.336.615-1.336 1.306Z" />
+                            </svg> English (fluent)
+                        </p>
+                    </div>
+                    <div className="vr" />
+                    <div>
+                        <h2><span className='highlight'>Hobbies </span></h2>
+                        <p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-square-fill" viewBox="0 0 16 16">
+                                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
+                            </svg> Listening to and discovering music
+                        </p>
+                        <p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-square-fill" viewBox="0 0 16 16">
+                                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
+                            </svg> Sports (football, padel, table tennis)
+                        </p>
+                        <p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-square-fill" viewBox="0 0 16 16">
+                                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2z" />
+                            </svg> Gaming
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/Offcanvas.js
+++ src/components/Offcanvas.js
@@ -1,61 +1,78 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from 'react-router-dom';
-import React, { useState } from "react";
 
 
 export default function Navigation() {
 
-    const [menuOpen, setMenuOpen] = useState(false)
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen)
-    }
-
-    const handleClose = () => setMenuOpen(false)
+    
     return (
-        <>
-            {[false].map((expand) => (
-                <Navbar key={expand} bg="dark" expand={expand} sticky='top' className='navbar navbar-dark bg-dark'>
-                    <Container fluid>
-                        <Navbar.Brand>
-                            <Link to="/">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="orangered" className="bi bi-file-code-fill" viewBox="0 0 16 16">
-                                    <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM6.646 5.646a.5.5 0 1 1 .708.708L5.707 8l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm2.708 0 2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 8 8.646 6.354a.5.5 0 1 1 .708-.708z" />
-                                </svg>&nbsp;
-                                <span className='title'> <strong>Hasan Marhoon </strong> </span>
-                            </Link>
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={toggleMenu} />
-                        <Navbar.Offcanvas
-                            className='text-bg-dark'
-                            id={`offcanvasNavbar-expand-${expand}`}
-                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                            placement="end"
-                            restoreFocus={false}
-                            show={menuOpen}
-                            onHide={handleClose}
-                        >
-                            <Offcanvas.Header closeButton closeVariant='white'>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-end align-items-space-between flex-grow-1 pe-3" id='nav' >
-                                    <Nav>
-                                        <Link to="/" onClick={toggleMenu}>Home</Link>
-                                    </Nav>
-                                    <Nav>
-                                        <Link to='/experience' onClick={toggleMenu}>
-                                            Practical expreience & Education
-                                        </Link>
-                                    </Nav>
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar >
-            ))
-            }
-        </>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky='top'>
+            <Container>
+                <Navbar.Brand id='navbar-title'>
+                    <Link to="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="orangered" className="bi bi-file-code-fill" viewBox="0 0 16 16">
+                            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM6.646 5.646a.5.5 0 1 1 .708.708L5.707 8l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm2.708 0 2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 8 8.646 6.354a.5.5 0 1 1 .708-.708z" />
+                        </svg>&nbsp;
+                        <span className='title'> <strong>Hasan Marhoon </strong> </span>
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className='me-auto'></Nav>
+                    <Nav id='nav'>
+                        <Nav>
+                            <Link to="/">About me</Link>
+                        </Nav>
+                        <Nav>
+                            <Link to="/experience">Education & Practical expreience</Link>
+                        </Nav>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
+        // <>
+        //     {[false].map((expand) => (
+        //         <Navbar key={expand} bg="dark" expand={expand} sticky='top' className='navbar navbar-dark bg-dark'>
+        //             <Container fluid>
+        //                 <Navbar.Brand>
+        //                     <Link to="/">
+        //                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="orangered" className="bi bi-file-code-fill" viewBox="0 0 16 16">
+        //                             <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM6.646 5.646a.5.5 0 1 1 .708.708L5.707 8l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm2.708 0 2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 8 8.646 6.354a.5.5 0 1 1 .708-.708z" />
+        //                         </svg>&nbsp;
+        //                         <span className='title'> <strong>Hasan Marhoon </strong> </span>
+        //                     </Link>
+        //                 </Navbar.Brand>
+        //                 <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={toggleMenu} className='d-lg-none'/>
+        //                 <Navbar.Offcanvas
+        //                     className='text-bg-dark'
+        //                     id={`offcanvasNavbar-expand-${expand}`}
+        //                     aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+        //                     placement="end"
+        //                     restoreFocus={false}
+        //                     show={menuOpen}
+        //                     onHide={handleClose}
+        //                 >
+        //                     <Offcanvas.Header closeButton closeVariant='white'>
+        //                     </Offcanvas.Header>
+        //                     <Offcanvas.Body>
+        //                         <Nav className="justify-content-end align-items-space-between flex-grow-1 pe-3" id='nav' >
+        //                             <Nav>
+        //                                 <Link to="/" onClick={toggleMenu}>Home</Link>
+        //                             </Nav>
+        //                             <Nav>
+        //                                 <Link to='/experience' onClick={toggleMenu}>
+        //                                     Practical expreience & Education
+        //                                 </Link>
+        //                             </Nav>
+        //                         </Nav>
+        //                     </Offcanvas.Body>
+        //                 </Navbar.Offcanvas>
+        //             </Container>
+        //         </Navbar >
+        //     ))
+        //     }
+        // </>
